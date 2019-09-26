7f0c5ce811c46a9040a27c443ee47503cc5660e7
ridwanmaulana8817 <ridwanmaulanacirebon@gmail.com>
2019-09-26 11:05:42 +0700

    Updates

---
+++ precache-manifest.99b804e68c2746f469ee1190d3d91355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e3db45f7a2a3b77e59db7c0ef561d4",
+    "url": "//github.com/ridwanmaulana8817/cv/index.html"
+  },
+  {
+    "revision": "3352052437d26397fed5",
+    "url": "//github.com/ridwanmaulana8817/cv/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e88dcfa4492700ef37e1",
+    "url": "//github.com/ridwanmaulana8817/cv/static/js/2.4b1a9693.chunk.js"
+  },
+  {
+    "revision": "3352052437d26397fed5",
+    "url": "//github.com/ridwanmaulana8817/cv/static/js/main.cf22d6c0.chunk.js"
+  },
+  {
+    "revision": "6bab1e54e6ec4d733d50",
+    "url": "//github.com/ridwanmaulana8817/cv/static/js/runtime-main.e2bd7ad6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "//github.com/ridwanmaulana8817/cv/static/media/logo.25bf045c.svg"
+  }
+]);
